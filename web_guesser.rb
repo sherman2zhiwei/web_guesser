20dd0ce483c65fca1823005d294cbdbfba0c2fa0
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2017-04-22 17:11:53 +0800

    Added background colors to indicate how close the guess is

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :SECRET_NUMBER, rand(100)
+SECRET_NUMBER = rand(100)
+set :color, 'white'
 message = ""
 
 def check_guess(guess)
 	if guess != nil and guess != ""
 		guess = guess.to_i
 		if guess - SECRET_NUMBER > 5
+			settings.color = 'red'
 			message = "Way too high!<p></p>"
 
 		elsif SECRET_NUMBER - guess > 5
+			settings.color = 'red'
 			message = "Way too low!<p></p>"
 
 		elsif guess - SECRET_NUMBER > 0
+			settings.color = '#FF6666'
 			message = "Too high!<p></p>"
 
 		elsif SECRET_NUMBER - guess > 0
+			settings.color = '#FF6666'
 			message = "Too low!<p></p>"
 
 		elsif SECRET_NUMBER == guess
+			settings.color = 'green'
 			message = "You got it right!<p></p>
 			THE SECRET NUMBER IS #{SECRET_NUMBER}"
 					
@@ -32,5 +38,5 @@ end
 get '/' do
 	guess = params['guess']	
 	message = check_guess(guess)
-	erb :index, :locals => {:message => message, :number => SECRET_NUMBER}
+	erb :index, :locals => {:message => message, :number => SECRET_NUMBER, :color => settings.color}
 end
